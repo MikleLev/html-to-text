d35f116622d4efe1aa4f39e04a3fb803d09af363
Mike <LevashovMisha@mail.ru>
2023-09-11 23:02:38 +0300

    2 commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        string htmlContent = "<html><body><table><tr><th>Header 1</th><th>Header 2</th></tr><tr><td>Data 1</td><td>Data 2</td></tr></table></body></html>";
+        string htmlContent = @"
+        <html>
+            <head>
+                <meta content='text/html; charset=unicode' http-equiv='Content-Type'>
+            </head>
+            <body>
+                <h1>Main Header</h1>
+                <p>This is a paragraph of text.</p>
+                <ul>
+                    <li>Bullet Point 1</li>
+                    <li>Bullet Point 2</li>
+                    <li>Bullet Point 3</li>
+                </ul>
+                <ol>
+                    <li>Numbered Item 1</li>
+                    <li>Numbered Item 2</li>
+                    <li>Numbered Item 3</li>
+                </ol>
+                <p>Visit <a href='https://example.com'>Example</a> website.</p>
+                <div>Highlighted Text</div>
+                <p>1 тест На листе 3 вынесен узел А, откуда он? Вынести его или дать ссылку на него.</p>
+                <div></div>
+                <div>2 На листе 7 примечания выполнить над основной надписью (см. ГОСТ 2.316-2008). Обращаю Ваше внимание, что на всех листах примечания д.б. выполнены над основной надписью шириной 185 мм</div>
+            </body>
+        </html>";
+
         string txtContent = ConvertHtmlToTxt(htmlContent);
         Console.WriteLine(txtContent);
     }
@@ -18,20 +43,62 @@ class Program
 
         StringBuilder txtContent = new StringBuilder();
 
-        foreach (var table in doc.DocumentNode.SelectNodes("//table"))
-        {
-            foreach (var row in table.SelectNodes("tr"))
-            {
-                foreach (var cell in row.SelectNodes("th|td"))
-                {
-                    string cellText = cell.InnerText.Trim();
-                    txtContent.Append(cellText + "\t"); // Добавляем табуляцию между ячейками
-                }
-                txtContent.AppendLine(); // Переходим на новую строку после каждой строки таблицы
-            }
-            txtContent.AppendLine(); // Добавляем пустую строку после каждой таблицы
-        }
+        ProcessNode(doc.DocumentNode, txtContent, false, null);
 
         return txtContent.ToString();
+    }
+
+    static void ProcessNode(HtmlNode node, StringBuilder txtContent, bool isList, string parentTag)
+    {
+        if (node.NodeType == HtmlNodeType.Text)
+        {
+            string text = node.InnerText.Trim();
+            if (!string.IsNullOrEmpty(text))
+            {
+                txtContent.AppendLine(text);
+            }
+        }
+        else if (node.Name == "a")
+        {
+            string linkText = node.InnerText.Trim();
+            string linkUrl = node.GetAttributeValue("href", "");
+            txtContent.AppendFormat("{0} ({1})", linkText, linkUrl);
+            txtContent.AppendLine();
+        }
+        else if (node.Name == "div")
+        {
+            txtContent.AppendLine(node.InnerText.Trim());
+        }
+        else
+        {
+            int index = 1;
+            foreach (var childNode in node.ChildNodes)
+            {
+                if (node.Name != "div")
+                {
+                    ProcessNode(childNode, txtContent, node.Name == "ul" || node.Name == "ol", node.Name);
+                }
+            }
+
+            if (node.Name == "ul" && parentTag != "li")
+            {
+                foreach (var listItem in node.SelectNodes(".//li"))
+                {
+                    txtContent.Append("• ");
+                    txtContent.AppendLine(listItem.InnerText.Trim());
+                }
+                txtContent.AppendLine();
+            }
+            else if (node.Name == "ol" && parentTag != "li")
+            {
+                foreach (var listItem in node.SelectNodes(".//li"))
+                {
+                    txtContent.AppendFormat("{0}. {1}", index, listItem.InnerText.Trim());
+                    txtContent.AppendLine();
+                    index++;
+                }
+                txtContent.AppendLine();
+            }
+        }
     }
 }
